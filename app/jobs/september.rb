a828018f8b00551cd0a1b31723c0810d3e46dd45
yvettecook <yvettecook@gmail.com>
2014-11-28 18:09:37 +0000

    Project widgets acting independently. For a time.

--- app/jobs/september.rb
+++ app/jobs/september.rb
@@ -1,15 +1,16 @@
 Dashing.scheduler.every '1s' do
 	project_value = get_project_status
-  Dashing.send_event('project', { value: project_value })
+  Dashing.send_event('project1', { value: project_value })
+	Dashing.send_event('project2', { value: rand(100) })
 end
 
 def get_project_status
 	project = Project.last
-	
+
 	project_status = 0
-	
+
 	milestones = [project.product, project.design, project.frontdev, project.backdev, project.presentation]
-	
+
 	milestones.each do |milestone|
 		project_status += 20 if milestone == true
 	end
